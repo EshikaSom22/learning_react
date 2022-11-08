46518aae97e17ae5404ca064d9d0c807d5d386ca
eshika <eshika22som2003@gmail.com>
2022-11-08 23:40:11 +0530

    hmm

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>
+      Netflix Series
+      </h1>
+      <p>Netflix and chill :)</p>
+      {/* this is not a html tag.it's a jsx expression */}
+      <ol>
+  <li>Mismatched </li>
+  <li>Mismatched Season 2</li>
+  <li>Money Heist </li>
+  <li>to all the boys i loved</li>
+  <li>365 Days</li>
+  </ol>
+      </>
+  )
+}
+
+// var h2=document.createElement("h2");
+// h2.innerHTML=" like";
+// document.getElementById('root').appendChild(h2);
+export default App
