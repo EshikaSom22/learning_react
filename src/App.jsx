c814d7acdb4e43c324787cd9301441a29dce1c3b
eshika <eshika22som2003@gmail.com>
2022-11-08 23:47:33 +0530

    hm

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
-
+const name="eshika som";
 const App = () => {
   return (
     <>
-      <h1>
-      Netflix Series
-      </h1>
-      <p>Netflix and chill :)</p>
-      {/* this is not a html tag.it's a jsx expression */}
-      <ol>
-  <li>Mismatched </li>
-  <li>Mismatched Season 2</li>
-  <li>Money Heist </li>
-  <li>to all the boys i loved</li>
-  <li>365 Days</li>
-  </ol>
+      <h1>my name is {name}</h1>
+      <p></p>
       </>
   )
 }
